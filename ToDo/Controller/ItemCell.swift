3152de1dfa255a5d826e50a30dcae51463f33254
gtokman <gtokman@fullsail.edu>
2016-12-22 13:54:49 -0500

    Added table view data source and tests

---
+++ ToDo/Controller/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  ToDo
+//
+//  Created by g tokman on 12/22/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+ 
+
+}
---
+++ ToDoTests/Controller/ItemListViewControllerTest.swift
@@ -0,0 +1,47 @@
+//
+//  ItemListViewControllerTest.swift
+//  ToDo
+//
+//  Created by g tokman on 12/22/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ItemListViewControllerTest: XCTestCase {
+    
+    var sut: ItemListViewController!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "ItemListViewController")
+        sut = viewController as! ItemListViewController
+        _ = sut.view
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    func test_TableView_AfterViewDidLoad_IsNotNil() {
+        XCTAssertNotNil(sut.tableView)
+    }
+    
+    func test_LoadingView_SetsTableViewDataSource() {
+        XCTAssertTrue(sut.tableView.dataSource is ItemListDataProvider)
+    }
+    
+    func test_LoadingView_SetsTableViewDelegate() {
+        XCTAssertTrue(sut.tableView.delegate is ItemListDataProvider)
+    }
+    
+    func test_LoadingView_SetsDataSourceAndDelegateToSameObject() {
+        XCTAssertEqual(sut.tableView.dataSource as? ItemListDataProvider,
+                       sut.tableView.delegate as? ItemListDataProvider)
+    }
+}

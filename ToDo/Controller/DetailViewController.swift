f2a181c0e60199a22764ad7bb1d75320b61b91cf
gtokman <gtokman@fullsail.edu>
2016-12-23 13:17:57 -0500

    Added detail view controller and tests to make sure outlets are set

---
+++ ToDo/Controller/DetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailViewController.swift
+//  ToDo
+//
+//  Created by g tokman on 12/23/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    
+    let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var locationNameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var mapView: MKMapView!
+    
+    var itemInfo: (itemManager: ItemManager, index: Int)?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let itemInfo = itemInfo else { return }
+        let item = itemInfo.itemManager.item(at: itemInfo.index)
+        
+        titleLabel.text = item.title
+        locationNameLabel.text = item.location?.name
+        descriptionLabel.text = item.itemDescription
+        
+        if let timestamp = item.timestamp {
+            let date = Date(timeIntervalSince1970: timestamp)
+            dateLabel.text = dateFormatter.string(from: date)
+        }
+        
+        if let coordinate = item.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coordinate, 100, 100)
+            mapView.region = region
+        }
+        
+    }
+    
+    func checkItem() {
+        if let itemInfo = itemInfo {
+            itemInfo.itemManager.checkItem(at: itemInfo.index)
+        }
+    }
+
+}
---
+++ ToDoTests/Controller/DetailViewControllerTests.swift
@@ -0,0 +1,96 @@
+//
+//  DetailViewControllerTests.swift
+//  ToDo
+//
+//  Created by g tokman on 12/23/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import CoreLocation
+import XCTest
+@testable import ToDo
+
+
+class DetailViewControllerTests: XCTestCase {
+    
+    var sut: DetailViewController!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        
+        _ = sut.view
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+  
+    func test_HasTitleLabel() {
+        XCTAssertNotNil(sut.titleLabel)
+    }
+    
+    func test_HasDateLabel() {
+        XCTAssertNotNil(sut.dateLabel)
+    }
+    
+    func test_HasLocationLabel() {
+        XCTAssertNotNil(sut.locationNameLabel)
+    }
+    
+    func test_DescriptionLabel() {
+        XCTAssertNotNil(sut.descriptionLabel)
+    }
+    
+    func test_HasMapView() {
+        XCTAssertNotNil(sut.mapView)
+    }
+    
+    func test_SettingItemInfo_SetsTextsToLabels() {
+        let coordinate = CLLocationCoordinate2DMake(51.2277, 6.7735)
+        
+        
+        let location = Location(name: "Foo", coordinate: coordinate)
+        let item = ToDoItem(title: "Bar",
+                            itemDescription: "Baz",
+                            timestamp: 1456150025,
+                            location: location)
+        
+        
+        let itemManager = ItemManager()
+        itemManager.add(item) 
+        
+        
+        sut.itemInfo = (itemManager, 0)
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
+        XCTAssertEqual(sut.titleLabel.text, "Bar")
+        XCTAssertEqual(sut.dateLabel.text, "02/22/2016")
+        XCTAssertEqual(sut.locationNameLabel.text, "Foo")
+        XCTAssertEqual(sut.descriptionLabel.text, "Baz")
+        XCTAssertEqualWithAccuracy(sut.mapView.centerCoordinate.latitude,
+                                   coordinate.latitude,
+                                   accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(sut.mapView.centerCoordinate.longitude,
+                                   coordinate.longitude,
+                                   accuracy: 0.001)
+    }
+    
+    func test_CheckItem_ChecksItemInItemManager() {
+        let itemManager = ItemManager()
+        itemManager.add(ToDoItem(title: "Foo"))
+        
+        
+        sut.itemInfo = (itemManager, 0)
+        sut.checkItem()
+        
+        
+        XCTAssertEqual(itemManager.toDoCount, 0)
+        XCTAssertEqual(itemManager.doneCount, 1) 
+    }
+    
+}

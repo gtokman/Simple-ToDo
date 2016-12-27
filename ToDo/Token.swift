cee6b664a4020d783d6634f75e36a6c33819a06f
gtokman <gtokman@fullsail.edu>
2016-12-26 19:30:25 -0500

    We used dependency injection to catch the completion handler of the session data task in our fake URL session. This way, we could feed test data into the implementation code and assert that the code is implemented as expected. As we controlled the data that the completion handler received, we were able to simulate all kinds of errors and drive the implementation of the correct error handling.

---
+++ ToDo/Token.swift
@@ -0,0 +1,15 @@
+//
+//  Token.swift
+//  ToDo
+//
+//  Created by g tokman on 12/25/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import Foundation
+
+struct Token {
+    
+    let id: String
+    
+}
--- ToDoTests/Controller/InputViewControllerTests.swift
+++ ToDoTests/Controller/InputViewControllerTests.swift
@@ -121,6 +121,36 @@ class InputViewControllerTests: XCTestCase {
         
         XCTAssertTrue(actions.contains("save")) 
     }
+    
+    func test_Geocoder_FetchesCoordinates() {
+        
+        let geocoderAnswered = expectation(description: "Geocoder")
+        
+        CLGeocoder().geocodeAddressString("Infinite Loop 1, Cupertino") { (placemarks, error) in
+            
+            
+            let coordinate = placemarks?.first?.location?.coordinate
+            
+            guard let latitude = coordinate?.latitude else {
+                XCTFail()
+                return
+            }
+
+            guard let longitude = coordinate?.longitude else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqualWithAccuracy(latitude, 37.3316,
+                                       accuracy: 0.0001)
+            XCTAssertEqualWithAccuracy(longitude, -122.030127,
+                                       accuracy: 0.0001)
+
+            geocoderAnswered.fulfill()
+        }
+        
+        waitForExpectations(timeout: 3, handler: nil)
+    }
 }
 
 extension InputViewControllerTests {
